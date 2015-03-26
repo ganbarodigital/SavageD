009d5fd0c0cdfd6914a99615f1838a4fc2e6e2d0
Stuart Herbert <stuherbert@ganbarodigital.com>
2015-03-26 10:03:02 +0000

    Fix: add missing includes and inherit statement

--- app/monitors/BaseMonitor.js
+++ app/monitors/BaseMonitor.js
@@ -1,11 +1,16 @@
 // Copyright (c) 2015-present Ganbaro Digital Ltd
 // All rights reserved
 
+// our third-party includes
+var util     = require("util");
+var dsCommon = require("dsCommon");
+
 function BaseMonitor(appServer, options) {
     // call our parent constructor
     BaseMonitor.super_.call(this, appServer, options);
 }
 module.exports = BaseMonitor;
+util.inherits(BaseMonitor, dsCommon.dsFeature);
 
 // ========================================================================
 //
@@ -15,4 +20,4 @@ module.exports = BaseMonitor;
 
 BaseMonitor.prototype.logRequest = function(req) {
     this.logInfo("HTTP " + req.method + "  " + req.url);
-}
+};
